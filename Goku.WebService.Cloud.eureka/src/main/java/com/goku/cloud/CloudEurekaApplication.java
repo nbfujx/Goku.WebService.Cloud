649dc94c9b3d8fcc565e7cd96f2ddae9f2d044b2
nbfujx <nbfujx@qq.com>
2018-01-30 10:17:28 +0800

    更新目录

---
+++ Goku.WebService.Cloud.eureka/src/main/java/com/goku/cloud/CloudEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.goku.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by nbfujx on 2017-11-24.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+}
---
+++ Goku.WebService.Cloud.zuul/src/main/java/com/goku/cloud/CloudZuulApplication.java
@@ -0,0 +1,18 @@
+package com.goku.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by nbfujx on 2018-01-30.
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class CloudZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZuulApplication.class, args);
+    }
+}
